9bcee50071d42060227bc6561cfcdfb2a05b58f5
Tim Heuer <tim@timheuer.com>
2017-10-04 15:27:44 -0700

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public List<string> GetNames()
+        {
+            return new List<string>() { "Tim", "Linda", "Chris", "Matt", "Immo" };
+        }
+    }
+}
---
+++ FunctionApp6/Function1.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace FunctionApp6
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+
+            var x = new ClassLibrary1.Class1();
+            var names = x.GetNames();
+
+            return req.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
